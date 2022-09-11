b046d5d748fa04563ae43d242d1b6b083dc442ed
Gabriela Oliveira <gabi.victoriasnt@hotmail.com>
2022-09-11 11:00:59 -0300

    Adding regex and validation if field is empty

--- src/pages/FifthyStage/index.jsx
+++ src/pages/FifthyStage/index.jsx
@@ -4,14 +4,25 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 import * as Styled from './styled';
 import { useNavigate } from 'react-router-dom';
+import { phoneMask } from '../../helpers/regex';
 
 const FifthyStage = () => {
   const [inputValue, setInputValue] = useState('');
   const navigate = useNavigate();
 
   const handleClick = () => {
-    localStorage.setItem("number", inputValue);
-    navigate('/last-stage');
+    if(inputValue){
+      localStorage.setItem("number", inputValue);
+      navigate('/last-stage');
+    }else{
+      alert("Este campo não pode ficar vazio!");
+    }
+  }
+
+  const handleText = (event) => {
+    const {value} = event.target;
+    event.target.value = phoneMask(value);
+    setInputValue(event.target.value);
   }
 
   return (
@@ -19,7 +30,7 @@ const FifthyStage = () => {
       <Styled.InfoArea>
         <Styled.WelcomeIcon src="src/assets/images/phone.png" />
 
-        <Input type="text" width="448px" height="40px" label="Essa experiência acontece toda pelo WhatsApp. Por isso, me diz qual o número do seu celular?" placeholder="Escreva seu whatsapp" onChange={(event) => setInputValue(event.target.value)} value={inputValue} description="Não se preocupe! Seu número será utilizado exclusivamente para que eu demonstre a experiência MIA" hasSpan="true"/>
+        <Input type="text" width="448px" height="40px" label="Essa experiência acontece toda pelo WhatsApp. Por isso, me diz qual o número do seu celular?" placeholder="Escreva seu whatsapp" onChange={(event) => handleText(event)} value={inputValue} description="Não se preocupe! Seu número será utilizado exclusivamente para que eu demonstre a experiência MIA" hasSpan="true"/>
 
         <Styled.ButtonArea>
           <Button text="Avançar" width="448px" height="48px" onClick={handleClick}/>
--- src/pages/FirstStage/index.jsx
+++ src/pages/FirstStage/index.jsx
@@ -4,14 +4,25 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 import * as Styled from './styled';
 import { useNavigate } from 'react-router-dom';
+import { onlyLetters } from '../../helpers/regex';
 
 const FirstStage = () => {
   const [inputValue, setInputValue] = useState('');
   const navigate = useNavigate();
 
   const handleClick = () => {
-    localStorage.setItem("name", inputValue);
-    navigate('/second-stage');
+    if(inputValue){
+      localStorage.setItem("name", inputValue);
+      navigate('/second-stage');
+    }else{
+      alert("Este campo não pode ficar vazio!");
+    }
+  }
+
+  const handleText = (event) => {
+    const {value} = event.target;
+    event.target.value = onlyLetters(value);
+    setInputValue(event.target.value);
   }
 
   return (
@@ -19,7 +30,7 @@ const FirstStage = () => {
       <Styled.InfoArea>
         <Styled.WelcomeIcon src="src/assets/images/welcome-icon.png" />
 
-        <Input type="text" width="448px" height="40px" label="Como você quer ser chamado?" placeholder="Escreva seu nome" onChange={(event) => setInputValue(event.target.value)} value={inputValue}/>
+        <Input type="text" width="448px" height="40px" label="Como você quer ser chamado?" placeholder="Escreva seu nome" onChange={(event) => {handleText(event)}} value={inputValue}/>
 
         <Styled.ButtonArea>
           <Button text="Avançar" width="448px" height="48px" onClick={handleClick}/>
